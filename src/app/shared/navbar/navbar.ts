8592a5cd7a7e1f89727a53b611812cefa4edb083
Jade Fauvelle <jfauvelle@devolutions.net>
2025-07-01 14:09:08 -0400

    Améliorer la navbar
    - Rajout du logo
    - Le logo route à home
    - hamburger en vue mobile

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -8,4 +8,10 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./navbar.css'],
   imports: [RouterModule]
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}

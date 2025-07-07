f68bf7ce46ee86b8bff09fb0d5ed235ddf4f3b26
Jade Fauvelle <jfauvelle@devolutions.net>
2025-07-07 14:20:40 -0400

    Users can click outside hamburger menu to close
    - They need to click outside of the nav for it to work
    - Added a TODO for later

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -10,6 +10,7 @@ import { RouterModule } from '@angular/router';
 })
 export class NavbarComponent {
   menuOpen = false;
+  constructor(private eRef: ElementRef) {}
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
@@ -17,5 +18,14 @@ export class NavbarComponent {
 
   closeMenu() {
     this.menuOpen = false;
+  }
+  //TODO: Fix it so it also works when the main nav (top) is clicked
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const clickedInside = this.eRef.nativeElement.contains(target);
+    if(this.menuOpen && !clickedInside) {
+      this.menuOpen = false;
+    }
   }
 }

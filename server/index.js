66cd8fb0f56871bfa2d2979dc02e8bae940b3d72
Jade Fauvelle <jfauvelle@devolutions.net>
2025-10-07 13:46:03 -0400

    Include new cover picture

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const auth = new google.auth.GoogleAuth({
 });
 
 const transporter = nodemailer.createTransport({
-  service: 'hotmail',
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
